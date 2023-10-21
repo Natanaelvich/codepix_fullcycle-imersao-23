161243e6147a0956c299dc3480da2b6eb4663f6f
natanaelvich <taelima1997@gmail.com>
2023-10-21 02:17:03 +0000

    add config env, typeorm and controllers

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BankAccount } from './models/bank-account.model';
+import { BankAccountController } from './controllers/bank-account/bank-account.controller';
+import { ConsoleModule } from 'nestjs-console';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    ConsoleModule,
+    TypeOrmModule.forRoot({
+      type: process.env.TYPEORM_CONNECTION as any,
+      host: process.env.TYPEORM_HOST,
+      port: parseInt(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [BankAccount],
+    }),
+    TypeOrmModule.forFeature([BankAccount]),
+  ],
+  controllers: [BankAccountController],
 })
 export class AppModule {}
