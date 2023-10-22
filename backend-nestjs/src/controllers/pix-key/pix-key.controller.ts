f39f8da4c7babb548c13d18be02e1e607c16d267
natanaelvich <taelima1997@gmail.com>
2023-10-22 01:25:57 +0000

    add controller pixkey

---
+++ backend-nestjs/src/controllers/pix-key/pix-key.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  ValidationPipe,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PixKeyDto } from 'src/dto/pix-key.dto';
+import { BankAccount } from 'src/models/bank-account.model';
+import { PixKey } from 'src/models/pix-key.model';
+import { Repository } from 'typeorm';
+
+@Controller('bank-accounts/:bankAccountId/pix-keys')
+export class PixKeyController {
+  constructor(
+    @InjectRepository(PixKey)
+    private pixKeyRepo: Repository<PixKey>,
+    @InjectRepository(BankAccount)
+    private bankAccountRepo: Repository<BankAccount>,
+  ) {}
+  @Get()
+  index(
+    @Param('bankAccountId', new ParseUUIDPipe({ version: '4' }))
+    bankAccountId: string,
+  ) {
+    return this.pixKeyRepo.find({
+      where: {
+        bank_account_id: bankAccountId,
+      },
+      order: {
+        created_at: 'DESC',
+      },
+    });
+  }
+
+  @Post()
+  async store(
+    @Param('bankAccountId', new ParseUUIDPipe({ version: '4' }))
+    bankAccountId: string,
+    @Body(new ValidationPipe({ errorHttpStatusCode: 422 }))
+    body: PixKeyDto,
+  ) {
+    await this.bankAccountRepo.findOneOrFail({ where: { id: bankAccountId } });
+
+    const pixKey = this.pixKeyRepo.create({
+      bank_account_id: bankAccountId,
+      ...body,
+    });
+    return await this.pixKeyRepo.save(pixKey);
+  }
+}
