1d1ab2ca22e8c3715d5cf65b279e3ce21481e1c7
natanaelvich <taelima1997@gmail.com>
2023-10-23 21:59:07 -0300

    add transaction to typeorm config

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -5,6 +5,7 @@ import { BankAccount } from './models/bank-account.model';
 import { BankAccountController } from './controllers/bank-account/bank-account.controller';
 import { ConsoleModule } from 'nestjs-console';
 import { PixKey } from './models/pix-key.model';
+import { Transaction } from './models/transaction.model';
 
 @Module({
   imports: [
@@ -17,9 +18,9 @@ import { PixKey } from './models/pix-key.model';
       username: process.env.TYPEORM_USERNAME,
       password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
-      entities: [BankAccount, PixKey],
+      entities: [BankAccount, PixKey, Transaction],
     }),
-    TypeOrmModule.forFeature([BankAccount, PixKey]),
+    TypeOrmModule.forFeature([BankAccount, PixKey, Transaction]),
   ],
   controllers: [BankAccountController],
 })
