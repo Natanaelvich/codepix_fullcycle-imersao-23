8b34fcbb0e9710d5f1d8be267f42feff85ec2500
natanaelvich <taelima1997@gmail.com>
2023-10-25 22:54:49 -0300

    add title component

---
+++ frontend-nextjs/src/components/Title/index.tsx
@@ -0,0 +1,15 @@
+import classes from './Title.module.scss';
+
+type Props = {
+    children: React.ReactNode;
+};
+
+const Title = (props: Props) => {
+    return (
+        <h1 className={classes.root}>
+            {props.children}
+        </h1>
+    );
+};
+
+export default Title;
