44551f4eae440662e6d575990d0d0dd4327e59d1
natanaelvich <taelima1997@gmail.com>
2023-10-25 22:56:14 -0300

    add layout component

---
+++ frontend-nextjs/src/components/Layout.tsx
@@ -0,0 +1,24 @@
+// @flow
+import * as React from "react";
+import { BankAccount } from "../model";
+import Footer from "./Footer";
+import MainContent from "./MainContent";
+import Navbar from "./Navbar";
+
+type Props = {
+    bankAccount: BankAccount;
+    children: React.ReactNode;
+};
+
+const Layout = (props: Props) => {
+  const { bankAccount } = props;
+  return (
+    <>
+      <Navbar bankAccount={bankAccount} />
+      <MainContent>{props.children}</MainContent>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
